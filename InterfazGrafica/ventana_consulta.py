0d55b0de5e3c127bf2f137df5da4d5ae289cec7a
JosafatZM <josafat.zamora4940@alumnos.udg.mx>
2024-11-10 01:40:45 -0600

    new feature

--- InterfazGrafica/ventana_consulta.py
+++ InterfazGrafica/ventana_consulta.py
@@ -2,6 +2,8 @@ import customtkinter as ctk
 from tkinter.font import BOLD
 from DataBase.data_base import BaseDatos as bd
 from DataBase.data_base import acceso_bd
+import numpy as np
+from scipy.interpolate import interp1d
 
 # para manipular la base de datos
 base_datos = bd(**acceso_bd)
@@ -84,7 +86,66 @@ class ConsultFrame(ctk.CTkToplevel):
 
 
     def label_button_frame_event(self, item):
-        print(f"label button frame clicked: {item}")
+        
+            print(f"label button frame clicked: {item}")
+            import matplotlib.pyplot as plt
+
+            query = f"SELECT emg FROM data_modular.datos_emg WHERE id_sujeto = '60'"
+            query2 = f"SELECT angulos_codo FROM data_modular.datos_angulos WHERE id_sujeto = '60'"
+
+            self.emg = base_datos.consulta(query)
+            self.valores_angulares= base_datos.consulta(query2)
+            
+            # tuple to list 
+            self.emg = self.emg[0][0]
+            self.emg = self.emg[1:-1].split(",")
+            self.emg = [float(x) for x in self.emg]
+
+            self.valores_angulares = self.valores_angulares[0][0]
+            self.valores_angulares = self.valores_angulares.strip('{}')
+            self.valores_angulares = self.valores_angulares.split(",")
+            self.valores_angulares = {key[1:-2]: float(value) for key, value in (pair.split(': ') for pair in self.valores_angulares)}
+            self.valores_angulares = list(self.valores_angulares.values())
+            print(len(self.valores_angulares))
+
+            # Calcular la duración de la señal en segundos (asumiendo una frecuencia de muestreo de 2000 Hz)
+            duracion_segundos = len(self.emg) / 2000
+            # Definir el rango de tiempo para el gráfico: desde 0 hasta la duración total en segundos
+            inicio_tiempo = 0
+            fin_tiempo = duracion_segundos
+            # El número total de muestras en la señal EMG
+            total_muestras = len(self.emg)
+            # Generar un vector de tiempo que se corresponde con cada muestra de la señal
+            # Utilizamos 'np.linspace' para crear una distribución uniforme de puntos de tiempo
+            vector_tiempo = np.linspace(inicio_tiempo, fin_tiempo, total_muestras)
+
+            fs = len(self.valores_angulares)/duracion_segundos
+            fin_tiempo= len(self.emg)/2000
+
+            # Determinar la longitud de los valores de codo y crear un vector de tiempo correspondiente
+            longitud_codo = len(self.valores_angulares)
+            vector_tiempo_codo = np.linspace(0, longitud_codo / fs, longitud_codo)  
+            # Crear la función de interpolación lineal para adaptar la señal de ángulo de codo al vector de tiempo de la señal EMG
+            interpolador_codo = interp1d(vector_tiempo_codo, self.valores_angulares, kind='linear', fill_value='extrapolate')
+            # Aplicar la interpolación para que los valores del codo se ajusten al vector de tiempo de la señal EMG
+            valores_codo_interpolados = interpolador_codo(vector_tiempo)
+
+            corte = 40000
+            fig, ax1 = plt.subplots(figsize=(10, 6))
+
+            ax1.set_xlabel('Time')
+            ax1.set_ylabel('EMG (V)')
+            ax1.plot(vector_tiempo[corte:], self.emg[corte:],
+                    label='Señal EMG', color='blue', linewidth=1)
+
+            ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+            ax2.set_ylabel('Angle (degrees)')
+            ax2.plot(vector_tiempo[corte:], valores_codo_interpolados[corte::],
+                    label='Variación ángulos de codo', color='green', linewidth=1)
+            plt.title('EMG and Angle Data')
+            plt.show()
+            
+        
 
     def buscar(self): 
             try:
